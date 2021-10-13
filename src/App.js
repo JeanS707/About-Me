3a57f092e63dd2aa8df7367ee5807caf3725b760
JeanS707 <74681689+JeanS707@users.noreply.github.com>
2021-10-13 18:39:29 -0400

    added (im funny)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello there I'm Jean Saldarriaga</h1>
-      <h2>I guess you could say "hello world" haha</h2>
+      <h2>I guess you could say "hello world" haha (im funny)</h2>
       <img src="https://s.yimg.com/uu/api/res/1.2/PKUdA3mFJdAbwQsVG_s_vw--~B/aD0zNTA7dz01NTQ7YXBwaWQ9eXRhY2h5b24-/http://media.zenfs.com/en-US/homerun/hello_giggles_454/e86652a2623ed17436b9acfc127095eb"></img>
       <a href="https://popcode.org/?snapshot=7a6717c3-1fea-43f8-b9ed-c36057905153">
         This was one of my first "sites"
