a6ebe28024b7641e3cccd9932538af437394b75f
JeanS707 <74681689+JeanS707@users.noreply.github.com>
2021-10-13 18:32:47 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello there I'm Jean Saldarriaga</h1>
+      <h2>I guess you could say "hello world" haha</h2>
+      <img src="https://s.yimg.com/uu/api/res/1.2/PKUdA3mFJdAbwQsVG_s_vw--~B/aD0zNTA7dz01NTQ7YXBwaWQ9eXRhY2h5b24-/http://media.zenfs.com/en-US/homerun/hello_giggles_454/e86652a2623ed17436b9acfc127095eb"></img>
+      <a href="https://popcode.org/?snapshot=7a6717c3-1fea-43f8-b9ed-c36057905153">
+        This was one of my first "sites"
+      </a>
+      <marquee>I love using this for no particular reason</marquee>
+      <p>
+        Also please listen to the Guilty Gear Strive Soundtrack I promise you'll
+        enjoy at least 1 of the songs!
+      </p>
+      <iframe
+        src="https://open.spotify.com/embed/album/3IaQ0DQMIXMShbMDNepeTK"
+        width="100%"
+        height="380"
+        frameBorder="0"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      ></iframe>
+    </div>
+  );
+}
